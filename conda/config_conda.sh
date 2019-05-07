c885283c208d45a320e07a164f1aaefbca45d884
ZizhouJia <jiazizhou@126.com>
2019-05-07 14:00:01 +0800

    add conda config

--- conda/config_conda.sh
+++ conda/config_conda.sh
@@ -1,1 +1,5 @@
+echo "export PATH=~/anaconda2/bin:$PATH" >> ~/.bashrc
+echo "export PATH=~/anaconda2/bin:$PATH" >> ~/.zshrc
+echo "export PATH=~/anaconda2/etc/profile.d/conda.sh:$PATH" >> ~/.bashrc
+echo "export PATH=~/anaconda2/etc/profile.d/conda.sh:$PATH" >> ~/.zshrc
 cp ./.condarc ~/.condarc
