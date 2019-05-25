6fca883ae6fbac537952d3204f2f6e4b82001c7c
ZizhouJia <jiazizhou@126.com>
2019-05-25 17:29:43 +0800

    change conda_config

--- conda/config_conda.sh
+++ conda/config_conda.sh
@@ -1,5 +1,5 @@
-echo "export PATH=~/anaconda3/bin:$PATH" >> ~/.bashrc
-echo "export PATH=~/anaconda3/bin:$PATH" >> ~/.zshrc
-echo "export PATH=~/anaconda3/etc/profile.d/conda.sh:$PATH" >> ~/.bashrc
-echo "export PATH=~/anaconda3/etc/profile.d/conda.sh:$PATH" >> ~/.zshrc
+echo 'export PATH=~/anaconda3/bin:$PATH' >> ~/.bashrc
+echo 'export PATH=~/anaconda3/bin:$PATH' >> ~/.zshrc
+echo 'export PATH=~/anaconda3/etc/profile.d/conda.sh:$PATH' >> ~/.bashrc
+echo 'export PATH=~/anaconda3/etc/profile.d/conda.sh:$PATH' >> ~/.zshrc
 cp ./.condarc ~/.condarc
