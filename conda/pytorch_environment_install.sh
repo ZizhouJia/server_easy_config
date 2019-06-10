0d13571aae1cc2ec1f0183fcac68fdba50087982
ZizhouJia <jiazizhou@126.com>
2019-06-10 06:52:53 -0400

    add isort

--- conda/pytorch_environment_install.sh
+++ conda/pytorch_environment_install.sh
@@ -1,21 +1,21 @@
-conda create -y -n python27jzz python=2.7
-conda activate python27jzz
-pip install tensorflow 
-pip install  tensorboard
-pip install sklearn
-pip install scikit-image
-pip install matplotlib
-pip install opencv-python
-pip install opencv-contrib-python
-pip install h5py
-pip install jedi
-pip install flake8
-pip install pep8
-pip install rawpy
-pip install nvidia-ml-py
-pip install tensorboardX
-pip install torchvision
-pip install torch
+#conda create -y -n python27jzz python=2.7
+#conda activate python27jzz
+#pip install tensorflow 
+#pip install  tensorboard
+#pip install sklearn
+#pip install scikit-image
+#pip install matplotlib
+#pip install opencv-python
+#pip install opencv-contrib-python
+#pip install h5py
+#pip install jedi
+#pip install flake8
+#pip install pep8
+#pip install rawpy
+#pip install nvidia-ml-py
+#pip install tensorboardX
+#pip install torchvision
+#pip install torch
 
 conda create -y -n python36jzz python=3.6
 conda activate python36jzz
@@ -30,6 +30,7 @@ pip install h5py
 pip install jedi
 pip install flake8
 pip install pep8
+pip install isort
 pip install rawpy
 pip insall nvidia-ml-py3
 pip install tensorboardX
