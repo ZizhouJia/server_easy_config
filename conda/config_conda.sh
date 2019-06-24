4fc56834498b8d243d94acf840cb9f549aae8474
ZizhouJia <jiazizhou@126.com>
2019-06-24 03:39:34 -0400

    change conda config

--- conda/config_conda.sh
+++ conda/config_conda.sh
@@ -2,4 +2,8 @@ echo 'export PATH=~/anaconda3/bin:$PATH' >> ~/.bashrc
 echo 'export PATH=~/anaconda3/bin:$PATH' >> ~/.zshrc
 echo 'export PATH=~/anaconda3/etc/profile.d/conda.sh:$PATH' >> ~/.bashrc
 echo 'export PATH=~/anaconda3/etc/profile.d/conda.sh:$PATH' >> ~/.zshrc
-cp ./.condarc ~/.condarc
+source ~/.bashrc
+source ~/.zshrc
+conda config --add channels https://mirrors.tuna.tsinghua.edu.cn/anaconda/pkgs/main/
+conda config --add channels https://mirrors.tuna.tsinghua.edu.cn/anaconda/pkgs/free/
+conda config --set show_channel_urls yes
---
+++ conda/download_conda_tsinghua.sh
@@ -0,0 +1,1 @@
+wget https://mirrors.tuna.tsinghua.edu.cn/anaconda/archive/Anaconda3-5.3.0-Linux-x86_64.sh
